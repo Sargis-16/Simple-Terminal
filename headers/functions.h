16b4e28e5f1479e4ee62e17854cd526b4af5be8c
Sargis-16 <95761493+Sargis-16@users.noreply.github.com>
2022-06-18 13:39:59 +0400

    Add files via upload

--- headers/functions.h
+++ headers/functions.h
@@ -1,1 +1,23 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <Shlwapi.h>
+#pragma comment(lib, "shlwapi.lib")
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <filesystem>
+#include <direct.h>
+#include <stdlib.h>
 
+std::string current_dir();
+
+std::string get_string(std::string prompt);
+
+std::string get_operation(std::string s);
+
+void handle(std::string operation, std::string whole);
+
+#endif //FUNCTIONS_H
